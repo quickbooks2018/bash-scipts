bffff9e6af5abb6f937d4ef7902539571c82b3c5
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2019-02-22 20:26:26 +0500

    aws-self-signed-acm

---
+++ aws-self-signed-acm.sh
@@ -0,0 +1,95 @@
+# Define where to store the generated certs and metadata.
+DIR="$(pwd)/tls"
+
+# Optional: Ensure the target directory exists and is empty.
+rm -rf "${DIR}"
+mkdir -p "${DIR}"
+
+# Create the openssl configuration file. This is used for both generating
+# the certificate as well as for specifying the extensions. It aims in favor
+# of automation, so the DN is encoding and not prompted.
+cat > "${DIR}/openssl.cnf" << EOF
+[req]
+default_bits = 2048
+encrypt_key  = no # Change to encrypt the private key using des3 or similar
+default_md   = sha256
+prompt       = no
+utf8         = yes
+# Speify the DN here so we aren't prompted (along with prompt = no above).
+distinguished_name = req_distinguished_name
+# Extensions for SAN IP and SAN DNS
+req_extensions = v3_req
+# Be sure to update the subject to match your organization.
+[req_distinguished_name]
+C  = CA
+ST = Ontario
+L  = The Cloud
+O  = IT
+CN = My Certificate
+# Allow client and server auth. You may want to only allow server auth.
+# Link to SAN names.
+[v3_req]
+basicConstraints     = CA:FALSE
+subjectKeyIdentifier = hash
+keyUsage             = digitalSignature, keyEncipherment
+extendedKeyUsage     = clientAuth, serverAuth
+subjectAltName       = @alt_names
+# Alternative names are specified as IP.# and DNS.# for IP addresses and
+# DNS accordingly. 
+[alt_names]
+IP.1  = 127.0.0.1
+DNS.1 = *.xxxxxxxxxxxxxxxxxxxx.com
+DNS.2 = localhost
+EOF
+
+# Create the certificate authority (CA). This will be a self-signed CA, and this
+# command generates both the private key and the certificate. You may want to 
+# adjust the number of bits (4096 is a bit more secure, but not supported in all
+# places at the time of this publication). 
+#
+# To put a password on the key, remove the -nodes option.
+#
+# Be sure to update the subject to match your organization.
+openssl req \
+  -new \
+  -newkey rsa:2048 \
+  -days 36500 \
+  -nodes \
+  -x509 \
+  -subj "/C=CA/ST=Ontario/L=The Cloud/O=CA" \
+  -keyout "${DIR}/CA.key" \
+  -out "${DIR}/CA.crt"
+#
+# For each server/service you want to secure with your CA, repeat the
+# following steps:
+#
+
+# Generate the private key for the service. Again, you may want to increase
+# the bits to 4096.
+openssl genrsa -out "${DIR}/xxxxxxxxxxxxxxxxxxxx.com.key" 2048
+
+# Generate a CSR using the configuration and the key just generated. We will
+# give this CSR to our CA to sign.
+openssl req \
+  -new -key "${DIR}/xxxxxxxxxxxxxxxxxxxx.com.key" \
+  -out "${DIR}/xxxxxxxxxxxxxxxxxxxx.com.csr" \
+  -config "${DIR}/openssl.cnf"
+  
+# Sign the CSR with our CA. This will generate a new certificate that is signed
+# by our CA.
+openssl x509 \
+  -req \
+  -days 3650 \
+  -in "${DIR}/xxxxxxxxxxxxxxxxxxxx.com.csr" \
+  -CA "${DIR}/CA.crt" \
+  -CAkey "${DIR}/CA.key" \
+  -CAcreateserial \
+  -extensions v3_req \
+  -extfile "${DIR}/openssl.cnf" \
+  -out "${DIR}/xxxxxxxxxxxxxxxxxxxx.com.crt"
+
+# (Optional) Verify the certificate.
+openssl x509 -in "${DIR}/xxxxxxxxxxxxxxxxxxxx.com.crt" -noout -text
+
+# Generate PFX For Windows 
+openssl pkcs12 -export -out xxxxxxxxxxxxxxxxxxxx.com.pfx -inkey xxxxxxxxxxxxxxxxxxxx.com.key -in xxxxxxxxxxxxxxxxxxxx.com.crt
